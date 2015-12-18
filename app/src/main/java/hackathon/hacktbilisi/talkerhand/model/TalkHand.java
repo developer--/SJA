03083c93ce5cb472a7ac47c6043c097d8b73f5d4
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2015-12-18 23:03:31 +0400

    init

---
+++ app/src/main/java/hackathon/hacktbilisi/talkerhand/model/TalkHand.java
@@ -0,0 +1,20 @@
+package hackathon.hacktbilisi.talkerhand.model;
+
+/**
+ * Created by jemo on 12/18/2015.
+ */
+public class TalkHand {
+    private String word;
+
+    public TalkHand(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
